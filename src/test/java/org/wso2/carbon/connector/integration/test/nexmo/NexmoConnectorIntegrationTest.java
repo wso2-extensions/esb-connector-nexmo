0c9c7548d270567f7f58a8b6bbb2bc4e1bf03526
srajah.shakila@gmail.com <srajah.shakila@gmail.com>
2016-07-13 17:15:18 +0530

    To release the tag

--- src/test/java/org/wso2/carbon/connector/integration/test/nexmo/NexmoConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/nexmo/NexmoConnectorIntegrationTest.java
@@ -45,7 +45,7 @@ public class NexmoConnectorIntegrationTest extends ConnectorIntegrationTestBase 
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("nexmo-connector-1.0.1-SNAPSHOT");
+        init("nexmo-connector-1.0.1");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
